5677d86748614f2a54f78096e8193b4139f1159f
Dalal-QA <sadouni,dalal@gmail.com>
2023-06-19 22:53:06 -0400

    testDoSearch using a data provider completed

--- src/test/java/data_providers/DataProviders.java
+++ src/test/java/data_providers/DataProviders.java
@@ -1,9 +1,20 @@
 package data_providers;
 
 import base.BasePage;
+import org.testng.annotations.DataProvider;
+import utils.ExcelData;
+
+import java.io.File;
 
 public class DataProviders extends BasePage {
 
+    String packagePath= System.getProperty("user.dir");
+    String dataPath= "src" + File.separator + "test" + File.separator + "resources" + File.separator + "test_data.xlsx";
+    ExcelData excelData = new ExcelData(packagePath + File.separator + dataPath);
+    @DataProvider(name ="testDoSearch")
+    public Object[][] getDoSearchProvider(){
 
+    return excelData.readStringArrays("testDoSearch");
+}
 
 }
--- src/test/java/test_search/TestSearch.java
+++ src/test/java/test_search/TestSearch.java
@@ -8,12 +8,19 @@ import org.testng.annotations.Test;
 
 public class TestSearch extends BasePage {
     Homepage homepage;
-    @Test
-    public void testDoSearch(){
-        homepage = new Homepage();
-        String[] searchTerms= excelData.readStringArray("testDoSearch");
-        String searchTerm = searchTerms[0];
+//    @Test
+//    public void testDoSearch(){
+//        homepage = new Homepage();
+//        String[] searchTerms= excelData.readStringArray("testDoSearch");
+//        String searchTerm = searchTerms[0];
+//
+//        SearchResultPage searchResultPage= homepage.doSearch(searchTerm);
+//        Assert.assertTrue(isElementVisible(searchResultPage.textSearchTerm));
+//    }
 
+    @Test(dataProviderClass = data_providers.DataProviders.class, dataProvider = "testDoSearch")
+    public void testDoSearch(String searchTerm){
+        homepage= new Homepage();
         SearchResultPage searchResultPage= homepage.doSearch(searchTerm);
         Assert.assertTrue(isElementVisible(searchResultPage.textSearchTerm));
     }
